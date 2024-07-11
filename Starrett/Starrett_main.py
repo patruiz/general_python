fea7a1c422d26cb77e59c882c566af2ba328ef1f
patruiz <142975437+patruiz@users.noreply.github.com>
2024-07-10 19:29:56 -0700

    updated starrett main to have a user input instead of changing code

--- Starrett/Starrett_main.py
+++ Starrett/Starrett_main.py
@@ -1,12 +1,16 @@
 import os 
 import pandas as pd 
+from pathlib import Path
 from Starrett_functions import *
 
 def main():
     os.system('cls')
+
+    file_dir = Path(input("File DIR: "))
+    file_name = f"{input('File Name: ')}.csv"
     
-    file_dir = r"C:\Users\pr19556\OneDrive - Applied Medical\Documents\Investigations\02 - MD Jaw Gap\Starrett Data\Device Variance\Raw"
-    file_name = "Maryland-Inside - MIS - Copy.CSV"
+    # file_dir = r"C:\Users\pr19556\OneDrive - Applied Medical\Documents\Investigations\02 - MD Jaw Gap\Starrett Data\1525034\Data\Processed"
+    # file_name = "Maryland-Inside - MIS - Copy.CSV"
     
     df_load = load_csv(file_dir, file_name)
     df_data = extract_data(df_load)
@@ -18,7 +22,7 @@ def main():
 
 def correlation():
     cols_to_read = ['Rear', 'Base', 'Mid', 'Tip']
-    inside_df = pd.read_csv(r"C:\Users\pr19556\OneDrive - Applied Medical\Documents\Investigations\02 - MD Jaw Gap\Starrett Data\1524590\Inside\1524590_Inside_PR.csv", usecols=cols_to_read)
+    inside_df = pd.read_csv(r"C:\Users\pr19556\OneDrive - Appl1525034_JawGap_Inside.csvied Medical\Documents\Investigations\02 - MD Jaw Gap\Starrett Data\1524590\Inside\1524590_Inside_PR.csv", usecols=cols_to_read)
     outside_df = pd.read_csv(r"C:\Users\pr19556\OneDrive - Applied Medical\Documents\Investigations\02 - MD Jaw Gap\Starrett Data\1524590\Outside\1524590_Outside_PR.csv", usecols=cols_to_read)
     perform_correlation_analysis(inside_df, outside_df)
     
