4e612d57d4254107b7a7c5d7d3359d71d75a19e1
patruiz <142975437+patruiz@users.noreply.github.com>
2024-09-30 12:18:48 -0700

    a lot of work on database

--- Inventory/main.py
+++ Inventory/main.py
@@ -3,25 +3,23 @@ from src.database import Database
 from src.inventory import Inventory
 
 def main():
-    # os.system('cls')
-    os.system('clear')
+    os.system('cls')
+    # os.system('clear')
 
     ass = Database(os.path.join(os.getcwd(), 'Inventory', 'data', 'swag.db'))
 
     ass.database_connect()
     ass.create_tables()
-    ass.load_filament_data()
-    ass.load_bom_data()
+
+    # ass.load_filament_data()
+    # ass.load_bom_data()
+    # ass.load_item_images()
+
+    # ass.view_image("1")
+
     # ass.show_changelog()
     
     
-    
-    # ass.load_data()
-
-    # inv = Inventory(ass)
-    # needtobuyswag = inv.get_parts((1, 3))
-    # inv.get_parts((1, 3))
-
 
 if __name__ == "__main__":
     main()
--- Inventory/src/database.py
+++ Inventory/src/database.py
@@ -8,6 +8,7 @@ class Database:
         self.database = None
         self.curr = None
         self.change_log = []
+        self.image_path = os.path.join(os.getcwd(), 'data', 'images')
 
     def database_connect(self):
         try:
@@ -29,6 +30,8 @@ class Database:
 
     def create_tables(self):
         if self.database and self.curr:
+
+            # Groups Table
             self.curr.execute(
                 """
                 CREATE TABLE IF NOT EXISTS Groups(
@@ -37,18 +40,8 @@ class Database:
                 )
                 """
             )
-            
-            self.curr.execute(
-                """
-                CREATE TABLE IF NOT EXISTS Items(
-                Item_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                Item_Name TEXT UNIQUE NOT NULL,
-                Group_ID INTEGER NOT NULL,
-                FOREIGN KEY (Group_ID) REFERENCES Groups(Group_ID)
-                )
-                """
-            )
-            
+
+            # Filament Table
             self.curr.execute(
                 """
                 CREATE TABLE IF NOT EXISTS Filament(
@@ -61,6 +54,20 @@ class Database:
                 """
             )
             
+            # Items Table
+            self.curr.execute(
+                """
+                CREATE TABLE IF NOT EXISTS Items(
+                Item_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                Item_Name TEXT UNIQUE NOT NULL,
+                Group_ID INTEGER NOT NULL,
+                Image_Path TEXT,
+                FOREIGN KEY (Group_ID) REFERENCES Groups(Group_ID)
+                )
+                """
+            )
+            
+            # Parts Table
             self.curr.execute(
                 """
                 CREATE TABLE IF NOT EXISTS Parts(
@@ -74,6 +81,7 @@ class Database:
                 """
             )
             
+            # BOM Table
             self.curr.execute(
                 """
                 CREATE TABLE IF NOT EXISTS BOM(
@@ -87,6 +95,58 @@ class Database:
                 """
             )
 
+            # # Inventory Table
+            # self.curr.execute(
+            #     """
+            #     CREATE TABLE IF NOT EXISTS Inventory(
+                
+            #     )
+            #     """
+            # )
+
+            # Orders Table
+            self.curr.execute(
+                """
+                CREATE TABLE IF NOT EXISTS Orders(
+                Order_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                Order_Date DATE NOT NULL,
+                Customer_ID INTEGER NOT NULL, 
+                Total VARCHAR NOT NULL
+                )
+                """
+            )
+
+            # Order Items Table
+            self.curr.execute(
+                """
+                CREATE TABLE IF NOT EXISTS OrderItems(
+                OrderItem_ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+                Order_ID INTEGER NOT NULL, 
+                Item_ID INTEGER NOT NULL,
+                Qty INTEGER NOT NULL, 
+                FOREIGN KEY (Item_ID) REFERENCES Items(Item_ID), 
+                FOREIGN KEY (Order_ID) REFERENCES Orders(Order_ID)
+                )
+                """
+            )
+
+            # Order Financials Table
+            self.curr.execute(
+                """
+                CREATE TABLE IF NOT EXISTS OrderFinancials(
+                Order_ID INTEGER PRIMARY KEY NOT NULL, 
+
+
+                FOREIGN KEY (Order_ID) REFERENCES Orders(Order_ID)
+
+                )
+                
+
+
+
+                """
+            )
+
     def _get_filament_id(self, color, fil_type, brand):
         self.curr.execute("""SELECT Filament_ID FROM Filament WHERE Filament_Color = ? AND Filament_Type = ? AND Filament_Brand = ?""", (color, fil_type, brand))
         return self.curr.fetchone()
@@ -96,40 +156,28 @@ class Database:
         return self.curr.fetchone()
 
     def _enter_group(self, group_name):
-        try:
-            self.curr.execute("""INSERT INTO Groups (Group_Name) VALUES (?)""", (group_name,))
-            print(f"Added Group: {group_name}")
-        except sqlite3.IntegrityError:
-            print(f"Group '{group_name}' already exists.")
+        self.curr.execute("""INSERT INTO Groups (Group_Name) VALUES (?)""", (group_name,))
+        print(f"Added Group: {group_name}")
 
     def _enter_filament_data(self, color, fil_type, brand):
-        try:
-            self.curr.execute("""INSERT INTO Filament (Filament_Color, Filament_Type, Filament_Brand) VALUES (?, ?, ?)""", (color, fil_type, brand))
-            print(f"Added Filament: {color}, {fil_type}, {brand}")
-        except sqlite3.IntegrityError:
-            print(f"Filament ({color}, {fil_type}, {brand}) already exists.")
+        self.curr.execute("""INSERT INTO Filament (Filament_Color, Filament_Type, Filament_Brand) VALUES (?, ?, ?)""", (color, fil_type, brand))
+        print(f"Added Filament: {color}, {fil_type}, {brand}")
                     
     def _get_part_id(self, part_type, variant, filament_id):
         self.curr.execute("""SELECT Part_ID FROM Parts WHERE (Part_Type = ? AND Part_Variant = ? AND Filament_ID = ?)""", (part_type, variant, filament_id))
         return self.curr.fetchone()
         
     def _enter_part(self, part_type, variant, filament_id):
-        try:
-            self.curr.execute("""INSERT INTO Parts (Part_Type, Part_Variant, Filament_ID) VALUES (?, ?, ?)""", (part_type, variant, filament_id))
-            print(f"Added Part: {part_type}, {variant}, {filament_id}")
-        except sqlite3.IntegrityError:
-            print(f"Part ({part_type}, {variant}, {filament_id}) already exists.")
+        self.curr.execute("""INSERT INTO Parts (Part_Type, Part_Variant, Filament_ID) VALUES (?, ?, ?)""", (part_type, variant, filament_id))
+        print(f"Added Part: {part_type}, {variant}, {filament_id}")
 
     def _get_item_id(self, item_name):
         self.curr.execute("""SELECT Item_ID FROM Items WHERE Item_Name = ?""", (item_name,))
         return self.curr.fetchone()
         
     def _enter_item(self, item_name, group_id):
-        try:
-            self.curr.execute("""INSERT INTO Items (Item_Name, Group_ID) VALUES (?, ?)""", (item_name, group_id))    
-            print(f"Added Item: {item_name} to group ID {group_id}")
-        except sqlite3.IntegrityError:
-            print(f"Item '{item_name}' already exists.")
+        self.curr.execute("""INSERT INTO Items (Item_Name, Group_ID) VALUES (?, ?)""", (item_name, group_id))    
+        print(f"Added Item: {item_name}")
 
     def _get_item_part(self, item_id, part_id):
         self.curr.execute("""SELECT Item_ID, Part_ID FROM BOM WHERE Item_ID = ? AND Part_ID = ?""", (item_id, part_id))
@@ -172,25 +220,54 @@ class Database:
                 qty = row['Qty']
 
                 fil_id = self._get_filament_id(color, fil_type, fil_brand)
+                if fil_id is None:
+                    self._enter_filament_data(color, fil_type, fil_brand)
+                    self._get_filament_id(color, fil_type, fil_brand)[0]
+                else:
+                    fil_id = fil_id[0]
 
-                group_id = self._get_group_id(group_name)[0]
+                group_id = self._get_group_id(group_name)
                 if group_id is None:
                     self._enter_group(group_name)
                     group_id = self._get_group_id(group_name)[0]
+                else:
+                    group_id = group_id[0]
                 
-                item_id = self._get_item_id(item_name)[0]
+                item_id = self._get_item_id(item_name)
                 if item_id is None:
                     self._enter_item(item_name, group_id)
                     item_id = self._get_item_id(item_name)[0]
+                else:
+                    item_id = item_id[0]
                 
-                print(item_id)
-                break
-                part_id = self._get_part_id(part_type, variant, fil_id[0])
+                part_id = self._get_part_id(part_type, variant, fil_id)
                 if part_id is None:
-                    self._enter_part(part_type, variant, fil_id[0])
-                    part_id = self._get_part_id(part_type, variant, fil_id[0])[0]
-                
-                if self._get_item_part(item_id, part_id) is None:
+                    self._enter_part(part_type, variant, fil_id)
+                    part_id = self._get_part_id(part_type, variant, fil_id)[0]
+                else:
+                    part_id = part_id[0]
+
+                part_item = self._get_item_part(item_id, part_id)
+                if part_item is None:
                     self._enter_item_part(item_id, part_id, qty)
+                    part_item = self._get_item_part(item_id, part_id)
                 
             self.database.commit()
+
+    def load_item_images(self):
+        self.curr.execute("""SELECT Item_ID FROM Items""")
+        item_ids = [item_id[0] for item_id in self.curr.fetchall()]
+        for item_id in item_ids:
+            name_format = f"I{item_id}.png"
+            img_path = os.path.join(self.image_path, name_format)
+            print(img_path)
+
+            self.curr.execute("""UPDATE Items SET Image_Path = ? WHERE Item_ID = ?""", (img_path, item_id))
+
+        self.database.commit()
+
+    def view_image(self, item_id):
+        self.curr.execute("""SELECT Image_Path FROM Items WHERE Item_ID = ?""", (item_id, ))
+        # open(self.curr.fetchone()[0])
+
+    
